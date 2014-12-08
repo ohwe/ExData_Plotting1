e424fe805ca423ea74d84250204871e756fb7900
Borislav Kozlovsky <ohw@yandex.ru>
2014-12-08 03:08:41 +0300

    added plot1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+yhh <- read.csv(file = "household_power_consumption.txt", sep = ';', 
+                colClasses = c(rep("character", 2), rep("numeric", 7) ), na.strings = c("?") )
+useful <- grepl('[12]/2/2007', yhh$Date)
+db <- yhh[useful,]
+rm(yhh)
+
+db$Time <- strptime(paste(db$Date,db$Time), format = "%d/%m/%Y %H:%M:%S")
+db$Date <- as.Date(db$Date, format = "%d/%m/%Y")
+
+
+png("plot1.png", width=480, height=480)
+hist(db$Global_active_power, main =  "Global active power", col = "red")
+dev.off()
