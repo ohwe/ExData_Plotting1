04790cba71e8ad5715d7a47b7973c7803abaf8be
Borislav Kozlovsky <ohw@yandex.ru>
2014-12-08 03:23:06 +0300

    plot1: fixed error

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 yhh <- read.csv(file = "household_power_consumption.txt", sep = ';', 
                 colClasses = c(rep("character", 2), rep("numeric", 7) ), na.strings = c("?") )
-useful <- grepl('[12]/2/2007', yhh$Date)
+useful <- grepl('^[12]/2/2007', yhh$Date)
 db <- yhh[useful,]
 rm(yhh)
 
@@ -11,3 +11,5 @@ db$Date <- as.Date(db$Date, format = "%d/%m/%Y")
 png("plot1.png", width=480, height=480)
 hist(db$Global_active_power, main =  "Global active power", col = "red")
 dev.off()
+
+
